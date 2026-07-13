7b2e23705f8a0ebd7dbc4d2aba7beb5b8127756b
Cedric-Accard <125440153+Cedric-Accard@users.noreply.github.com>
2026-07-13 15:22:38 +0200

    Fix2g

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import remarkMath from 'remark-math';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://cedricaccard.github.io',
-	base: process.env.BASE_PATH || '/astro-scholar',
+	base: process.env.BASE_PATH || '/',
 	integrations: [
 		mdx(),
 		sitemap()
